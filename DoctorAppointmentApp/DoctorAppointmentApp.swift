20862aa1f5e2572e7b96afcfe4a5457086f313c5
Manish Jawale <133796470+manishj9@users.noreply.github.com>
2025-05-03 13:52:51 +0530

    chore: edited and corrected code in ContentView\.swift and DoctoreAppointmentApp.swift

--- DoctorAppointmentApp/DoctorAppointmentApp.swift
+++ DoctorAppointmentApp/DoctorAppointmentApp.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import UIKit
+import FirebaseCore
 
+class AppDelegate: NSObject , UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 @main
-struct DoctorAppointmentAppApp: App {
+struct DoctorAppointmentApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView1()
+        }
+    }
+}
+
+struct ContentView1: View {
+    @AppStorage("hasCompletedOnboarding") var hasCompletedOnboarding = false
+    init() {
+            #if DEBUG
+            // Reset onboarding for testing (only in debug builds)
+            UserDefaults.standard.removeObject(forKey: "hasCompletedOnboarding")
+            print("Onboarding reset for development")
+            #endif
+        }
+    var body: some View {
+        Group {
+            if hasCompletedOnboarding {
+                SignInView()
+            } else {
+                OnboardingView()
+            }
         }
     }
 }
